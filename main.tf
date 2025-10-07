9d7077adf6563732b9797eba7431a022fb30b45b
gauravgaikkwad13 <gauravgaikkwad@gmail.com>
2025-10-07 20:31:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-02d26659fd82cf299"
-instance_type = "t2.medium"
-key_name = "demoproject1"
-vpc_security_group_ids = ["sg-0cdd4aaa4687f110c"]
+ami = "ami-052064a798f08f0d3"
+instance_type = "t3.small"
+key_name = "jenkins"
+vpc_security_group_ids = ["sg-0f5196ef065be7d3c"]
 tags = {
 Name = var.instance_names[count.index]
 }
