f7d37e249a70b13315a7ea81b8ef16d598da9efa
Gaurav Gaikwad <gauravgaikkwad@gmail.com>
2026-01-04 10:12:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0c398cb65a93047f2"
+ami = "ami-0ecb62995f68bb549"
 instance_type = "c7i-flex.large"
 key_name = "terraform"
 vpc_security_group_ids = ["sg-0e029b6d99b1ebece"]
